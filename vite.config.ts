8bedd4d8abf9e627ae4c211a024940a31d5c8f8d
Jeongyeun <jeongyeongim508@gmail.com>
2025-10-29 15:38:01 +0900

    백엔드 서버에 연동

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+      proxy: {
+        // '/api'로 시작하는 모든 요청을
+        '/api': {
+          // 백엔드 서버 'http://localhost:8080'으로 대신 보내줌
+          target: 'http://localhost:8080',
+
+          // CORS 오류를 피하기 위해 'origin' 헤더를 변경
+          changeOrigin: true,
+        },
+      },
+    },
 });
