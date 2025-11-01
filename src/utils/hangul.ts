d5a4730d15a45ac1ff744547b1960c88d23b5e14
jiho <jiho30125@gmail.com>
2025-11-01 17:06:33 +0900

    extract vowels

---
+++ src/utils/hangul.ts
@@ -0,0 +1,18 @@
+import { disassembleCompleteCharacter, combineVowels } from 'es-hangul';
+
+export function extractVowels(sentence: string) {
+  return sentence
+    .split('')
+    .filter(e => e != ' ')
+    .map(extractVowel);
+}
+
+function extractVowel(word: string) {
+  const disassembled = disassembleCompleteCharacter(word);
+  if (!disassembled) return null;
+  const vowel = disassembled.jungseong;
+  if (!vowel) return null;
+  if (vowel.length == 1) return vowel;
+  const [v1, v2] = vowel.split('');
+  return combineVowels(v1, v2);
+}
